2d935d14f1b8d017cc716508d11213d84dacb9a4
adrien <adrien@DeenLaptop>
2018-12-20 13:26:35 +0100

    Third commit

--- src/main/java/com/ulco/calculatrice/App.java
+++ src/main/java/com/ulco/calculatrice/App.java
@@ -12,17 +12,18 @@ public class App
 	
     public static void main( String[] args )
     {
-    	int nbre1 = 0, nbre2 = 0;
+    	int nbre1 = 0;
+    	int nbre2 = 0;
     	
-        System.out.println( "Entrer le premier nombre:" );
-        nbre1 = clavier.nextInt();
+    	System.out.println( "Entrer le premier nombre:" );
+    	nbre1 = clavier.nextInt();
         
-        System.out.println( "Entrer le second nombre:" );
-        nbre2 = clavier.nextInt();
+    	System.out.println( "Entrer le second nombre:" );
+    	nbre2 = clavier.nextInt();
         
-       System.out.println("La somme de " + nbre1 + " & " + nbre2 + " est: " + (nbre1+nbre2));;
-       System.out.println("La ssoustraction de " + nbre1 + " & " + nbre2 + " est: " + (nbre1-nbre2));
-       System.out.println("La multiplication de " + nbre1 + " & " + nbre2 + " est: " + (nbre1*nbre2));
-       System.out.println("La division de " + nbre1 + " & " + nbre2 + " est: " + (nbre1/nbre2));
+    	System.out.println("La somme de " + nbre1 + " & " + nbre2 + " est: " + (nbre1+nbre2));;
+    	System.out.println("La soustraction de " + nbre1 + " & " + nbre2 + " est: " + (nbre1-nbre2));
+    	System.out.println("La multiplication de " + nbre1 + " & " + nbre2 + " est: " + (nbre1*nbre2));
+    	System.out.println("La division de " + nbre1 + " & " + nbre2 + " est: " + (nbre1/nbre2));
     }
 }
